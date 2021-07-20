7714cf77e84097bb8154bbaf92f237fcef498221
Matthew Flores <matthew.flores8@gmail.com>
2021-07-20 13:14:10 -0400

    Add files via upload

--- contactform.php
+++ contactform.php
@@ -1,19 +1,31 @@
-<?php
+    <?php
 
-if (isset($_POST["submit"])) {
-    
-    $name = $_POST["name"];
-    $mailFrom = $_POST["email"];
-    $message = $_POST["message"];
-    
-    $mailTo = "info@matthew-flores.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "New Message from ".$name.".\n\n".$message;
-    
-    mail($mailTo, $txt, $headers);
-    
-    header("Location: index.php?mailsend");
+$name = $_POST['name'];
+$email = $_POST['email'];
+$select = $_POST['select'];
+$message = $_POST['message'];
+
+$formcontent="From: $name \n Interested in: $select \n Message: $message";
+
+if ($select == 'license') { 
+    $to = 'information@mysite.com';
+}
+else if (select == 'athletes') { 
+    $to = 'product@mysite.com'; 
+}
+else if (select == 'grouplicensing') { 
+    $to = 'career@mysite.com'; 
+}
+else if (select == 'investment') { 
+    $to = 'career@mysite.com'; 
+}
+else { //other options
+    $to = 'others@mysite.com'; 
 }
 
+$subject = "Enquiry from Website";
+$mailheader = "From: $email \r\n";
+mail($to, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You! <br /> We will get in touch with you as soon as possible.";
 
 ?>
