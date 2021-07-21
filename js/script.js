4144076dda6b36f7fbc15e42b7a4a8ea40e2a13d
Matthew Flores <matthew.flores8@gmail.com>
2021-07-20 21:35:05 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
  //Replace Text function		
  $(function () {
      count = 0;
-     wordsArray = ["CREATES OPPORTUNITIES.", "MAKES MOMENTS HAPPEN.", "CHANGES THE GAME."];
+     wordsArray = ["CREATES OPPORTUNITIES", "MAKES MOMENTS HAPPEN", "CHANGES THE GAME"];
      setInterval(function () {
          count++;
          $("#word").fadeOut(1250, function () {
@@ -22,3 +22,40 @@
  });
  //End Replace Text function
 
+
+ //Form Validation
+   const constraints = {
+       name: {
+           presence: { allowEmpty: false }
+       },
+       email: {
+           presence: { allowEmpty: false },
+           email: true
+       },
+       message: {
+           presence: { allowEmpty: false }
+       }
+   };
+
+   const form = document.getElementById('contact-form');
+
+   form.addEventListener('submit', function (event) {
+     const formValues = {
+         name: form.elements.name.value,
+         email: form.elements.email.value,
+         message: form.elements.message.value
+     };
+
+     const errors = validate(formValues, constraints);
+
+     if (errors) {
+       event.preventDefault();
+       const errorMessage = Object
+           .values(errors)
+           .map(function (fieldValues) { return fieldValues.join(', ')})
+           .join("\n");
+
+       alert(errorMessage);
+     }
+   }, false);
+ //End Form Validation
